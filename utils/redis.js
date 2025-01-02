290a0d116f9b0b0770fa994bacb8b67c670a4e8b
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2025-01-02 09:04:03 +0200

    h

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,12 @@
+import { promisify } from 'util';
 import { createClient } from 'redis';
 
 class RedisClient {
   constructor() {
     this.client = createClient();
     this.client.on('error', (err) => console.log('Redis Client Error', err));
+
+    this.getAsync = promisify(this.client.GET).bind(this);
   }
 
   isAlive() {
@@ -11,7 +14,7 @@ class RedisClient {
   }
 
   async get(key) {
-    return this.client.get(key);
+    return this.getAsync(key);
   }
 
   async set(key, value, duration) {
