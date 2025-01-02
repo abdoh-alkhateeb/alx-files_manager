b194d60e172569c46a23e88dc87f7036ef7698f6
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2025-01-02 07:34:00 +0200

    x

--- utils/redis.js
+++ utils/redis.js
@@ -3,7 +3,7 @@ import { createClient } from 'redis';
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.client.on_error((err) => console.log('Redis Client Error', err));
+    this.client.on('error', (err) => console.log('Redis Client Error', err));
   }
 
   isAlive() {
